11c8aaf1992682a06f7eb5e44f5e90b21674518c
worldgreatkim <edwin.dkim@gmail.com>
2025-06-12 22:19:59 +0900

    String 불변객체

---
+++ 중급1/java-mid1-practice/src/lang/string/immutable/StringImmutable1.java
@@ -0,0 +1,10 @@
+package lang.string.immutable;
+
+public class StringImmutable1 {
+    public static void main(String[] args) {
+        String str = "hello";
+        String str2 = str.concat(" java");
+        System.out.println("str = " +str);
+        System.out.println("str2 = " +str2);
+    }
+}
